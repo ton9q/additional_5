1fc25323ccac2aaa7737d6eeb12cf6800ebf6690
ton9q <kuchma_anton@mail.ru>
2018-09-13 01:17:14 +0300

    addiction of logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  var stack = [];
+  if (str.length % 2 != 0) {
+    return false;
+  }
+
+  for(let i = 0; i < str.length; i++) {
+    for(let j = 0; j < bracketsConfig.length; j++) {
+      
+      if (bracketsConfig[j][0] != bracketsConfig[j][1]) { // brackets are not the same
+        if(str[i] == bracketsConfig[j][0]) {
+          stack.push(str[i]);  
+        }
+        
+        else if(str[i] == bracketsConfig[j][1]) {
+          if (stack[stack.length-1] == bracketsConfig[j][0]) {
+            stack.pop();
+          }
+        }
+      }
+
+      else if (bracketsConfig[j][0] == bracketsConfig[j][1]) { // brackets are the same
+        if(str[i] == bracketsConfig[j][0]) {
+          if (!stack.length || stack[stack.length-1] != bracketsConfig[j][0]) {
+            stack.push(str[i]);
+          }
+          
+          else if (stack[stack.length-1] == bracketsConfig[j][0]) {
+            stack.pop();
+          }
+        }
+      }
+    }
+  }
+
+  if(stack.length) return false;
+
+  return true;
+};
